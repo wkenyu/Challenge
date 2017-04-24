ee078082999d6b6a6e0bd5617495dee6a78f2ac0
kenyu <w.kenyu@gmail.com>
2017-04-24 21:39:15 +0900

    Complete all tasks

--- run_analysis.R
+++ run_analysis.R
@@ -1,12 +1,40 @@
-xtrain <- fread("~/DataScientist/UCI HAR Dataset/train/X_train.txt")
-xtest <- fread("~/DataScientist/UCI HAR Dataset/test/X_test.txt")
+library(plyr)
+library(data.table)
+library(dplyr)
+
+if(!exists("xtrain"))
+  xtrain <- fread("~/DataScientist/UCI HAR Dataset/train/X_train.txt")
+if(!exists("xtest"))
+    xtest <- fread("~/DataScientist/UCI HAR Dataset/test/X_test.txt")
 
 mergedData <- bind_rows(xtrain, xtest)
 
 features <- fread("~/DataScientist/UCI HAR Dataset/features.txt")
 features <- transpose(features[,2])
-           
 
+valid_column_name <-  make.names(as.character(unname(features)), unique = TRUE)
+names(mergedData) <- valid_column_name
 matched <- grep("std|mean[^Freq]", features)
 
 selected <- select(mergedData, matched)
+
+activity_labels <- read.table("~/DataScientist/UCI HAR Dataset/activity_labels.txt")
+names(activity_labels) <- c("ID", "activity")
+
+ytrain <- fread("~/DataScientist/UCI HAR Dataset/train/y_train.txt")
+ytest <- fread("~/DataScientist/UCI HAR Dataset/test/y_test.txt")
+
+mergedLabel <- bind_rows(ytrain, ytest)
+
+names(mergedLabel) <- "ID"
+
+selected <- mutate(selected, activity = activity_labels[match(mergedLabel$ID, activity_labels$ID),2])
+
+subject_train <- read.table("~/DataScientist/UCI HAR Dataset/train/subject_train.txt")
+subject_test <- read.table("~/DataScientist/UCI HAR Dataset/test/subject_test.txt")
+
+mergedSubject <- bind_rows(subject_train, subject_test)
+names(mergedSubject) <- "subject"
+
+selected <- cbind(selected, mergedSubject)
+result <- ddply(selected, .(activity, subject), colwise(mean))
